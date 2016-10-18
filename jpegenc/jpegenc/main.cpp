65d98fda830ad31932d0074edc8f77414c19caed
relikd <info@relikd.de>
2016-10-18 14:15:10 +0200

    first Xcode C++ proj

---
+++ jpegenc/jpegenc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jpegenc
+//
+//  Created by Oleg on 18/10/16.
+//  Copyright © 2016 FHWS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
