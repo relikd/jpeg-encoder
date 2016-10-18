96724b5889a2b69a5c1f7d00e839c3aa5ccd7b64
Oleg Geier <oleg@relikd.de>
2016-10-18 21:35:42 +0200

    spaces -> tabs ppm max value wird aufgezogen auf 255 (8bit)

--- jpegenc/jpegenc/PPMLoader.cpp
+++ jpegenc/jpegenc/PPMLoader.cpp
@@ -9,40 +9,40 @@
 #include "PPMLoader.hpp"
 
 std::shared_ptr<Image> PPMLoader::load() {
-    
-    FILE* file = fopen(this->file, "r");
-    
-    /*if(file == nullptr) {
-        std::cout << "File does not exist" << std::endl;
-        
-        return Image(0, 0);
-    }*/
-    
-    char magicNumber[255];
-    fscanf(file, "%s\n", magicNumber);
-    
-    
-    int width, height, maxValue;
-    
-    fscanf(file, "%d %d\n", &width, &height);
-    fscanf(file, "%d\n", &maxValue);
-    
-    auto image = std::make_shared<Image>(width, height);
-    
-    size_t index = 0;
-    
-    while(1){
-        
-        int r,g,b;
-        
-        int elementsRead = fscanf(file, "%d %d %d", &r, &g, &b);
-        
-        if(elementsRead < 3){
-            break;
-        }
-        
-        image->setPixel(index++, Pixel(r,g,b));
-    }
-    
-    return image;
+	
+	FILE* file = fopen(this->file, "r");
+	
+	/*if(file == nullptr) {
+		std::cout << "File does not exist" << std::endl;
+		
+		return Image(0, 0);
+	}*/
+	
+	char magicNumber[255];
+	fscanf(file, "%s\n", magicNumber);
+	
+	
+	int width, height, maxValue;
+	
+	fscanf(file, "%d %d\n", &width, &height);
+	fscanf(file, "%d\n", &maxValue);
+	
+	auto image = std::make_shared<Image>(width, height);
+	
+	size_t index = 0;
+	
+	while(1){
+		
+		int r,g,b;
+		
+		int elementsRead = fscanf(file, "%d %d %d", &r, &g, &b);
+		
+		if(elementsRead < 3){
+			break;
+		}
+		
+		image->setPixel(index++, Pixel(r,g,b).set_highest_to_255_from(maxValue));
+	}
+	
+	return image;
 }
--- jpegenc/jpegenc/main.cpp
+++ jpegenc/jpegenc/main.cpp
@@ -13,11 +13,11 @@
 int main(int argc, const char * argv[]) {
 	// insert code here...
 	std::cout << "Hello, World!\n";
-    
-    PPMLoader loader("data/very_small.ppm");
-    
-    auto image = loader.load();
-    
-    image->print();
-    return 0;
+	
+	PPMLoader loader("data/very_small.ppm");
+	
+	auto image = loader.load();
+	
+	image->print();
+	return 0;
 }
