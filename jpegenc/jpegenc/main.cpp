4127dcb9eade59c5b2164311f3fcfa54267c9dd3
Marvin Therolf <marvin.therolf@gmail.com>
2016-10-21 14:24:19 +0200

    Implement reducing by subsampling

--- jpegenc/jpegenc/main.cpp
+++ jpegenc/jpegenc/main.cpp
@@ -9,12 +9,15 @@ int main(int argc, const char *argv[]) {
 	auto image = loader.load("/home/marv/Projects/jpeg-encoder/jpegenc/jpegenc/data/very_small.ppm");
 	image->print();
 
-	RGBToYCbCrConverter converter1;
-	image = converter1.convert(image);
-	image->print();
+//	RGBToYCbCrConverter converter1;
+//	image = converter1.convert(image);
 
-	YCbCrToRGBConverter converter2;
-	image = converter2.convert(image);
+//	YCbCrToRGBConverter converter2;
+//	image = converter2.convert(image);
+
+	image->reduceBySubSamplingChannel1(2);
+	image->reduceBySubSamplingChannel2(2);
+	image->reduceBySubSamplingChannel3(2);
 	image->print();
 
 	return 0;
