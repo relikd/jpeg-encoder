987a5ab3695c5d58e0f6cfe5dc11e00f24aae0fc
Christian Braun <chbr@adorsys.de>
2016-11-06 18:22:46 +0100

    Add 4k image

--- jpegenc/jpegenc/main.cpp
+++ jpegenc/jpegenc/main.cpp
@@ -3,15 +3,24 @@
 #include "converter/RGBToYCbCrConverter.hpp"
 #include "converter/YCbCrToRGBConverter.h"
 
+#include <time.h>
+
 int main(int argc, const char *argv[]) {
 
 	PPMLoader loader;
-	auto image = loader.load("data/very_small.ppm");
-//	auto image = loader.load("/home/marv/Projects/jpeg-encoder/jpegenc/jpegenc/data/very_small.ppm");
-	image->print();
+	clock_t t;
+	t = clock();
+	auto image = loader.load("data/singapore4k.test.ppm");
+	
 
-//	RGBToYCbCrConverter converter1;
-//	image = converter1.convert(image);
+//	auto image = loader.load("/home/marv/Projects/jpeg-encoder/jpegenc/jpegenc/data/very_small.ppm");
+	//image->print();
+
+	RGBToYCbCrConverter converter1;
+	image = converter1.convert(image);
+	
+	t = clock() - t;
+	printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
 
 //	YCbCrToRGBConverter converter2;
 //	image = converter2.convert(image);
@@ -24,7 +33,7 @@ int main(int argc, const char *argv[]) {
 //	image->channel2->reduceByAveraging(2);
 //	image->channel3->reduceByAveraging(2);
 
-	image->print();
+	//image->print();
 
 	return 0;
 }
