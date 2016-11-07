71b5fbca6603dc2c4db48ddbc78918b011c25676
Christian Braun <chbr@adorsys.de>
2016-11-07 18:43:31 +0100

    Improve normalize in ppm and remove unused methods in ppm loader

--- jpegenc/jpegenc/io/PPMLoader.hpp
+++ jpegenc/jpegenc/io/PPMLoader.hpp
@@ -8,13 +8,11 @@
 struct PPMLoader {
 	PPMLoader() {}
 	
-	std::shared_ptr<Image> load(const char *pathToImage);
 	std::shared_ptr<Image> customLoad(const char *pathToImage);
 	void write(const char *pathToImage, std::shared_ptr<Image> image);
 
 private:
 	color normalize(color colorValue, int originalMaxValue, int normalizedMaxValue);
-	int	scanForPattern(FILE * file, const char * fmt , void* arg0, void* arg1 = nullptr, void* arg2 = nullptr);
 };
 
 #endif /* PPMLoader_hpp */
