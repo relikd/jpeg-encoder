6b64d4bee377389605c43ca5fb71d27b09619133
Marvin Therolf <marvin.therolf@gmail.com>
2016-11-07 14:32:47 +0100

    Apply group session results

--- jpegenc/jpegenc/main.cpp
+++ jpegenc/jpegenc/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "loader/PPMLoader.hpp"
+#include "io/PPMLoader.hpp"
 #include "converter/RGBToYCbCrConverter.hpp"
 #include "converter/YCbCrToRGBConverter.h"
 
@@ -10,25 +10,37 @@ int main(int argc, const char *argv[]) {
 	PPMLoader loader;
 	clock_t t;
 	t = clock();
-	auto image = loader.load("data/singapore4k.test.ppm");
-	
 
-//	auto image = loader.load("/home/marv/Projects/jpeg-encoder/jpegenc/jpegenc/data/very_small.ppm");
+	auto image = loader.customLoad("/home/marv/Projects/jpeg-encoder/jpegenc/jpegenc/data/singapore4k.test.ppm");
+
+	std::cout << (size_t) image->channel1->getValue( 0, image->imageSize.height - 1, image->imageSize ) << std::endl;
+
+	RGBToYCbCrConverter converter1;
+	image = converter1.convert(image);
+
+	std::cout << (size_t) image->channel1->getValue( 0, image->imageSize.height - 1, image->imageSize ) << std::endl;
+
+	image->channel2->reduceBySubSampling( image->imageSize.width, image->imageSize.height );
+	image->channel3->reduceBySubSampling( image->imageSize.width, image->imageSize.height );
+
+	std::cout << (size_t) image->channel1->getValue( 0, image->imageSize.height - 1, image->imageSize ) << std::endl;
+
+	YCbCrToRGBConverter converter2;
+	image = converter2.convert(image);
+
+	std::cout << (size_t) image->channel1->getValue( 0, image->imageSize.height - 1, image->imageSize ) << std::endl;
+
+	loader.write("/home/marv/Projects/jpeg-encoder/jpegenc/jpegenc/data/output.test.ppm", image);
+
+//	auto image = loader.load("data/singapore4k.test.ppm");
 //	image->print();
 
-//	RGBToYCbCrConverter converter1;
-//	image = converter1.convert(image);
-	
+
 	t = clock() - t;
 	printf ("It took me %lu clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
 
-//	YCbCrToRGBConverter converter2;
-//	image = converter2.convert(image);
-
-//	image->reduceBySubSample(2,2);
-//	image->reduceByAvarage(2,2);
-	
 //	image->print();
+
 
 	return 0;
 }
