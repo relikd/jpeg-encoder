d18da3f7b28a0dd2063668f55ba0f7aafac46f70
relikd <info@relikd.de>
2016-11-09 00:39:25 +0100

    Super Duper Parser... gehts noch schneller?!

--- jpegenc/jpegenc/io/PPMLoader.hpp
+++ jpegenc/jpegenc/io/PPMLoader.hpp
@@ -1,18 +1,23 @@
 #ifndef PPMLoader_hpp
 #define PPMLoader_hpp
 
-#include <iostream>
-#include <memory>
 #include "../model/Image.hpp"
 
-struct PPMLoader {
+class PPMLoader {
+public:
 	PPMLoader() {}
-	
 	std::shared_ptr<Image> load(const char *pathToImage);
 	void write(const char *pathToImage, std::shared_ptr<Image> image);
 
 private:
-	color normalize(color colorValue, int originalMaxValue, int normalizedMaxValue);
+	long filesize;
+	char *buffer; // the whole file content
+	
+	color normalize(color colorValue, const unsigned int originalMaxValue, const unsigned int normalizedMaxValue);
+	
+	void readFileToMemory(const char *pathToImage);
+	bool parseHeader(size_t &index, size_t &width, size_t &height, unsigned int &maxValue);
+	void parseData(size_t &index, std::shared_ptr<Image> image, const unsigned int maxValue);
 };
 
 #endif /* PPMLoader_hpp */
