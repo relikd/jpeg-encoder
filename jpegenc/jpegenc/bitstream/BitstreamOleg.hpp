9acb24ea58c1fc1559ef3aa48ec6c3c7310985e0
Oleg Geier <oleg@relikd.de>
2016-11-10 08:29:43 +0100

    Generic performance test class. Example in main testImage() and testOleg()

--- jpegenc/jpegenc/bitstream/BitstreamOleg.hpp
+++ jpegenc/jpegenc/bitstream/BitstreamOleg.hpp
@@ -14,7 +14,7 @@ const size_t BLOCK_SIZE  = 0x80000;  // = 524288   =  2^19
 
 
 class BitstreamOleg {
-
+	
 	int allocatedPages = 1;
 	size_t index = 0; // 32bit: 10:19:3
 	std::vector<BitChar *> blocks;
@@ -24,6 +24,8 @@ public:
 	BitstreamOleg() {
 		blocks.push_back(new BitChar[BLOCK_SIZE]);
 	}
+	
+	size_t numberOfBits() { return index; }
 	
 	void add( const bool bit );
 	void add( const BitChar byte, short amount );
--- jpegenc/jpegenc/main.cpp
+++ jpegenc/jpegenc/main.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
+#include <stdlib.h>
 #include "io/PPMLoader.hpp"
 #include "converter/RGBToYCbCrConverter.hpp"
 #include "converter/YCbCrToRGBConverter.hpp"
-#include <stdlib.h>
+#include "helper/Test.hpp"
+
 #include "bitstream/BitstreamMarcel.hpp"
 #include "bitstream/BitstreamMarv.hpp"
 #include "bitstream/BitstreamOleg.hpp"
 
+
+//  ---------------------------------------------------------------
+// |
+// |  PPM Image Processing
+// |
+//  ---------------------------------------------------------------
+
 void testImage() {
-	PPMLoader loader;
-	clock_t t;
-	t = clock();
-
-	auto image = loader.load("data/gigantic.test.ppm");
-
-	RGBToYCbCrConverter converter1;
-	image = converter1.convert(image);
-
-	image->channel2->reduceBySubSampling( image->imageSize.width, image->imageSize.height );
-	image->channel3->reduceBySubSampling( image->imageSize.width, image->imageSize.height );
-
-	YCbCrToRGBConverter converter2;
-	image = converter2.convert(image);
-
-	loader.write("data/output.test.ppm", image);
-
-	t = clock() - t;
-	printf ("It took me %lu clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+	std::cout << "Loading image ..." << std::endl;
+	Test::performance([]{
+		PPMLoader loader;
+		auto image = loader.load("data/singapore4k.test.ppm");
+		
+//		RGBToYCbCrConverter converter1;
+//		image = converter1.convert(image);
+//		
+//		image->channel2->reduceBySubSampling( image->imageSize.width, image->imageSize.height );
+//		image->channel3->reduceBySubSampling( image->imageSize.width, image->imageSize.height );
+//		
+//		YCbCrToRGBConverter converter2;
+//		image = converter2.convert(image);
+//		
+//		loader.write("data/output.test.ppm", image);
+	});
 }
+
+//  ---------------------------------------------------------------
+// |
+// |  Bitstream
+// |
+//  ---------------------------------------------------------------
 
 void testChris() {
 }
@@ -139,63 +151,53 @@ void testOleg() {
 	bitstream.add('l', 8); // 0110 1100
 	bitstream.add('D', 8); // 0100 0100
 	bitstream.add(' ', 8); // 0010 0000
-//	for (size_t i = 0; i < 131072*8-2; ++i) {
+//	for (size_t i = 0; i < 524288*8-2; ++i) {
 //		bitstream.add(1);
 //	}
 	bitstream.add(255, 6);
 //	bitstream.add(0);
-	bitstream.saveToFile("data/out.txt");
+//	bitstream.saveToFile("data/out.txt");
 	bitstream.add(0);
 	bitstream.add(1);
 	bitstream.add(0);
-	bitstream.saveToFile("data/out2.txt");
+//	bitstream.saveToFile("data/out2.txt");
 //	bitstream.print();
 	
 	
-	// Test performance adding bits
-	size_t numberOfRounds = 100;
-	size_t numberOfElements = 10000000;
-	clock_t timeStamp;
-	clock_t totalTime = 0;
+	//  there is a significant amount of overhead even for an empty test (0.034109 seconds)
+	//testPerformance(10000000, 100, []{}, []{});
 	
-	for (size_t i = 0; i < numberOfRounds; ++i)
-	{
-		timeStamp = clock();
-		
-		BitstreamOleg bitStream;
-		for (size_t k = 0; k < numberOfElements; ++k)
-		{
-			bitStream.add(true);
-		}
-		timeStamp = clock() - timeStamp;
-		totalTime = totalTime + timeStamp;
-	}
-	clock_t averageTime = totalTime / numberOfRounds;
+	std::cout << "Testing, Oleg" << std::endl;
+	BitstreamOleg testStream;
 	
-	printf("Adding %lu single bits took %lu clicks (%f seconds) on average (%lu times).\n",numberOfElements, averageTime,((float)averageTime)/CLOCKS_PER_SEC, numberOfRounds);
+	std::cout << "Write single bit: "; // (0.074520 seconds)
+	Test::performance(10000000, 10, [&testStream]{ testStream = BitstreamOleg(); }, [&testStream]{
+		testStream.add(1);
+	});
 	
-	// Test performance adding bytes
-	numberOfRounds = 100;
-	numberOfElements = 10000000;
-	totalTime = 0;
+	std::cout << "Write byte bits: "; // (0.126076 seconds)
+	Test::performance(10000000, 10, [&testStream]{
+		testStream = BitstreamOleg(); // init
+		testStream.add(1);
+	}, [&testStream]{
+		testStream.add((char) 0xd2, 8); // 11010010
+	});
 	
-	for (size_t i = 0; i < numberOfRounds; ++i)
-	{
-		timeStamp = clock();
-		
-		BitstreamOleg bitStream;
-//		bitStream.add(1);
-		for (size_t k = 0; k < numberOfElements; ++k)
-		{
-			bitStream.add((char) 0xd2, 8); // 11010010
-		}
-		timeStamp = clock() - timeStamp;
-		totalTime = totalTime + timeStamp;
-	}
-	averageTime = totalTime / numberOfRounds;
-	
-	printf("Adding %lu bytes took %lu clicks (%f seconds) on average (%lu times).\n",numberOfElements, averageTime,((float)averageTime)/CLOCKS_PER_SEC, numberOfRounds);
+	std::cout << "Read single bit: "; // (0.087629 seconds)
+	size_t maxRead = testStream.numberOfBits() - 2;
+	size_t idx = 0;
+	Test::performance(10000000, 10, []{}, [&testStream, &idx, maxRead]{
+		testStream.read(idx++);
+		if (idx > maxRead)
+			idx = 0;
+	});
 }
+
+// ################################################################
+// #
+// #  Main
+// #
+// ################################################################
 
 int main(int argc, const char *argv[]) {
 //	testImage();
