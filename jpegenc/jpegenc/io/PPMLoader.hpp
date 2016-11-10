6d4f49fae357b590935a5e990aa41add4b6b942f
relikd <info@relikd.de>
2016-11-10 12:25:04 +0100

    Faster PPM writer

--- jpegenc/jpegenc/io/PPMLoader.hpp
+++ jpegenc/jpegenc/io/PPMLoader.hpp
@@ -16,9 +16,14 @@ private:
 	
 	color normalize(color colorValue, const unsigned int originalMaxValue, const unsigned int normalizedMaxValue);
 	
+	// read
 	void readFileToMemory(const char *pathToImage);
 	bool parseHeader(size_t &index, size_t &width, size_t &height, unsigned int &maxValue);
 	void parseData(size_t &index, std::shared_ptr<Image> image, const unsigned int maxValue);
+	
+	// write
+	void readNumberToFileSaveBuffer(size_t number, char *buf, unsigned short &index);
+	void readNumberToFileSaveBuffer(unsigned int &zahl, unsigned short &stellen, char *buf, unsigned short &index, unsigned int &zehnerPotenz);
 };
 
 #endif /* PPMLoader_hpp */
