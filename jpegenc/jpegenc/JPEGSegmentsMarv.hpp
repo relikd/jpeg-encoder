d5a38da524c96795a0e837149328ddbcbca0ad5f
Marvin Therolf <marvin.therolf@enowa.ag>
2016-11-17 15:26:51 +0100

    Create package marvSegmente

---
+++ jpegenc/jpegenc/JPEGSegmentsMarv.hpp
@@ -0,0 +1,14 @@
+//
+//  JPEGSegmentsMarv.hpp
+//  jpegenc
+//
+//  Created by Marvin Therolf on 17/11/2016.
+//  Copyright © 2016 FHWS. All rights reserved.
+//
+
+#ifndef JPEGSegmentsMarv_hpp
+#define JPEGSegmentsMarv_hpp
+
+#include <stdio.h>
+
+#endif /* JPEGSegmentsMarv_hpp */
