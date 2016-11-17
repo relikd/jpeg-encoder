080ab0a24e806f91325028a560efce48f21ee028
Marvin Therolf <marvin.therolf@enowa.ag>
2016-11-17 16:01:38 +0100

    Joined forces with Chris

--- jpegenc/jpegenc/JPEGSegmentsMarv.hpp
+++ jpegenc/jpegenc/JPEGSegmentsMarv.hpp
@@ -1,14 +1,35 @@
-//
-//  JPEGSegmentsMarv.hpp
-//  jpegenc
-//
-//  Created by Marvin Therolf on 17/11/2016.
-//  Copyright © 2016 FHWS. All rights reserved.
-//
-
 #ifndef JPEGSegmentsMarv_hpp
 #define JPEGSegmentsMarv_hpp
 
 #include <stdio.h>
+#include "Bitstream.hpp"
 
 #endif /* JPEGSegmentsMarv_hpp */
+
+struct Segment {
+    uint16_t marker;
+    
+    Segment(uint16_t marker) : marker(marker) {
+    }
+    virtual void write(Bitstream *bitStream);
+};
+
+struct TestSegment : Segment {
+
+    TestSegment() : Segment(0xFFFF) {
+    }
+    
+};
+
+struct SegmentWriter {
+    Bitstream *bitStream;
+    
+    SegmentWriter() {
+    }
+    
+    ~SegmentWriter() {
+    }
+    
+    void addSegment(Segment *segment);
+    void writeToFile(const char *pathToFile);
+};
