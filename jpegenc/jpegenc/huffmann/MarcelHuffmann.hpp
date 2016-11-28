bc8126516e2fec7f77f9b5c2a95d4be7811de665
Marcel Groß <mgr@adorsys.de>
2016-11-28 11:17:00 +0100

    Add huffmann

---
+++ jpegenc/jpegenc/huffmann/MarcelHuffmann.hpp
@@ -0,0 +1,52 @@
+//
+//  MarcelHuffmann.hpp
+//  jpegenc
+//
+//  Created by Marcel Groß on 22.11.16.
+//  Copyright © 2016 FHWS. All rights reserved.
+//
+
+#ifndef MarcelHuffmann_hpp
+#define MarcelHuffmann_hpp
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+struct InputWord {
+    int word;
+    int amount;
+    
+    InputWord(int word) : word(word), amount(0) {}
+    
+    void increase();
+    
+    bool operator < (const InputWord& input) const {
+        return (amount < input.amount);
+    }
+    
+    bool operator == (const InputWord& input) const {
+        return (amount == input.amount);
+    }
+};
+
+class MarcelHuffmann {
+    std::vector<InputWord> words;
+    
+public:
+    MarcelHuffmann() {}
+    
+    void addToWords(std::vector<int>);
+    
+private:
+
+    void sortByAppearance();
+};
+
+
+
+
+
+
+
+#endif /* MarcelHuffmann_hpp */
