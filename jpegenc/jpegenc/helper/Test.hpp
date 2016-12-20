4a2870a4736725b9d633fb30e866147c47b2b821
relikd <info@relikd.de>
2016-12-20 02:03:05 +0100

    Performance Seconds

--- jpegenc/jpegenc/helper/Test.hpp
+++ jpegenc/jpegenc/helper/Test.hpp
@@ -2,9 +2,26 @@
 #define Test_hpp
 
 #include <functional>
+#include <chrono>
+
+class Timer {
+public:
+	Timer() : beg_(clock_::now()) {}
+	void reset() { beg_ = clock_::now(); }
+	double elapsed() const {
+		return std::chrono::duration_cast<second_>
+		(clock_::now() - beg_).count(); }
+	
+private:
+	typedef std::chrono::high_resolution_clock clock_;
+	typedef std::chrono::duration<double, std::ratio<1> > second_;
+	std::chrono::time_point<clock_> beg_;
+};
+
 
 class Test {
 public:
+	static void howManyOperationsInSeconds(size_t seconds, const char* description, std::function<size_t(bool &shouldExecute)> func);
 	static void performance(std::function<void()> func);
 	static void performance(const size_t iterations, const size_t rounds, std::function<void(size_t numberOfElements)> func);
 };
