cacb43952ea85f91067867cb7f623804126d16bb
Oleg Geier <oleg@relikd.de>
2016-12-21 23:56:18 +0100

    Channel with block size + Refactoring -> 2.5x faster!

--- jpegenc/jpegenc/model/Image.cpp
+++ jpegenc/jpegenc/model/Image.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include "Image.hpp"
 
-//std::ostream& operator<<(std::ostream& out, const color value){
-//	return out << (size_t)value;
-//}
 
 std::ostream& operator<<(std::ostream& out, const ColorSpace value){
 	switch (value) {
@@ -13,35 +10,59 @@ std::ostream& operator<<(std::ostream& out, const ColorSpace value){
 }
 
 void Image::print(bool useIntValues) {
-	std::cout << "Color space: " << colorSpace;
+	std::cout << "Color space: " << colorSpace << std::endl;
+	
+	setReadingRuleForAllChannel();
+	
 	size_t numberOfPixels = imageSize.pixelCount;
 	for (size_t i = 0; i < numberOfPixels; ++i) {
-		if (i % imageSize.width == 0)
-			std::cout << std::endl;
 		
 		if (useIntValues) {
-			std::cout << (int)(channel1->getValue(i, imageSize)*255 + 0.5f) << "\t";
-			std::cout << (int)(channel2->getValue(i, imageSize)*255 + 0.5f) << "\t";
-			std::cout << (int)(channel3->getValue(i, imageSize)*255 + 0.5f) << "\t\t";
+			std::cout << (int)(channel1->readNextValue()*255 + 0.5f) << "\t";
+			std::cout << (int)(channel2->readNextValue()*255 + 0.5f) << "\t";
+			std::cout << (int)(channel3->readNextValue()*255 + 0.5f) << "\t\t";
 		} else {
-			std::cout << (channel1->getValue(i, imageSize)) << "\t";
-			std::cout << (channel2->getValue(i, imageSize)) << "\t";
-			std::cout << (channel3->getValue(i, imageSize)) << "\t\t";
+			std::cout << channel1->readNextValue() << "\t";
+			std::cout << channel2->readNextValue() << "\t";
+			std::cout << channel3->readNextValue() << "\t\t";
+		}
+		
+		if (i % imageSize.width == imageSize.width - 1) {
+			std::cout << std::endl;
+			size_t removeAdditionalColumns = channel1->imageSize.width - imageSize.width;
+			while (removeAdditionalColumns--) {
+				channel1->readNextValue(); // skip if image doesnt fit block size
+				channel2->readNextValue();
+				channel3->readNextValue();
+			}
 		}
 	}
-	std::cout << std::endl << std::endl;
+	std::cout << std::endl;
 }
 
 void Image::reduceBySubSample(size_t xdiv, size_t ydiv) {
 	channel1->reduceBySubSampling(xdiv, ydiv);
 	channel2->reduceBySubSampling(xdiv, ydiv);
 	channel3->reduceBySubSampling(xdiv, ydiv);
-	imageSize /= Dimension(xdiv,ydiv);
+	imageSize = Dimension(imageSize.width / xdiv, imageSize.height / ydiv);
 }
 
 void Image::reduceByAverage(size_t xdiv, size_t ydiv) {
 	channel1->reduceByAveraging(xdiv, ydiv);
 	channel2->reduceByAveraging(xdiv, ydiv);
 	channel3->reduceByAveraging(xdiv, ydiv);
-	imageSize /= Dimension(xdiv,ydiv);
+	imageSize = Dimension(imageSize.width / xdiv, imageSize.height / ydiv);
 }
+
+void Image::seekAllChannelToStart(ChannelSeekType type) {
+	channel1->seekTo(type);
+	channel2->seekTo(type);
+	channel3->seekTo(type);
+}
+
+void Image::setReadingRuleForAllChannel() {
+	channel1->setReadIndexAccessMappingRule(imageSize);
+	channel2->setReadIndexAccessMappingRule(imageSize);
+	channel3->setReadIndexAccessMappingRule(imageSize);
+}
+
