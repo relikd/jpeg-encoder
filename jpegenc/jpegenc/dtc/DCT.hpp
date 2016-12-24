757ce6f8b65cfc734c002d936a3cf7825c13f96f
relikd <info@relikd.de>
2016-12-24 21:25:11 +0100

    Improved separated matrix multiplication 3.7x faster

--- jpegenc/jpegenc/dtc/DCT.hpp
+++ jpegenc/jpegenc/dtc/DCT.hpp
@@ -8,6 +8,7 @@ struct DCT {
 	static Mat transform(Mat input);
 	static void transform(float* &input, float* &output, size_t width, size_t height);
 	static Mat transform2(Mat input);
+	static void transform2(float* &input);
 	static Mat inverse(Mat input);
 private:
 	static Mat generateA(int dimension);
