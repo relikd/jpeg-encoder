9cba119245f7424bc729dd1f4c54bbbab175ac3e
relikd <info@relikd.de>
2016-12-25 14:18:49 +0100

    Nice little Arai tweak will bring us the first place (CPU competition)

--- jpegenc/jpegenc/dtc/Arai.cpp
+++ jpegenc/jpegenc/dtc/Arai.cpp
@@ -27,110 +27,21 @@
  16 x lesender Zugriff auf Array
   8 x schreibender Zugriff auf Array
 
- */
+*/
 
 void Arai::transformLine(float **x) {
-    float a0 = *x[0] + *x[7];
-    float a1 = *x[1] + *x[6];
-    float a2 = *x[2] + *x[5];
-    float a3 = *x[3] + *x[4];
-    float a5 = *x[2] - *x[5];
-    float a6 = *x[1] - *x[6];
-    float a7 = *x[0] - *x[7];
-    
-    float b0 = a0 + a3;
-    float b1 = a1 + a2;
-    float b3 = a0 - a3;
-    float b4 = -(*x[3] - *x[4]) - a5;
-    float b6 = a6 + a7;
-    
-    float A5_block = (b4 + b6) * A5;
-    
-    float d2 = ((a1 - a2) + b3) * A1;
-    float d4 = -(b4 * A2) - A5_block;
-    float d5 = (a5 + a6) * A3;
-    float d6 = (b6 * A4) - A5_block;
-    
-    float e5 = d5 + a7;
-    float e7 = a7 - d5;
-    
-    *x[0] = (b0 + b1) * S0;
-    *x[1] = (e5 + d6) * S1;
-    *x[2] = (d2 + b3) * S2;
-    *x[3] = (e7 - d4) * S3;
-    *x[4] = (b0 - b1) * S4;
-    *x[5] = (d4 + e7) * S5;
-    *x[6] = (b3 - d2) * S6;
-    *x[7] = (e5 - d6) * S7;
-}
-
-Mat Arai::transform(Mat matrix) {
-    processRows(matrix.values);
-    processColumns(matrix.values);
-    return matrix;
-}
-
-void Arai::transformMT(float* values) {
-	processRows(values);
-	processColumns(values);
-}
-
-void Arai::processRows(float *values) {
-    for (int row = 0; row < 8; ++row)
-    {
-        float **currentLine = new float*[8];
-        
-        for (int column = 0; column < 8; ++column)
-        {
-            currentLine[column] = &values[row * 8 + column];
-        }
-        transformLine(currentLine);
-        
-        delete[] currentLine;
-    }
-}
-
-void Arai::processColumns(float *values) {
-    for (int column = 0; column < 8; ++column)
-    {
-        float **currentLine = new float*[8];
-        
-        for (int row = 0; row < 8; ++row)
-        {
-            currentLine[row] = &values[row * 8 + column];
-        }
-        transformLine(currentLine);
-        
-        delete[] currentLine;
-    }
-}
-
-
-
-
-void Arai::transformLineOG(float* &x, size_t mat_width) {
-	// TODO: We could make two functions, this multiplication is only needed for columns
-	float &idx0 = x[0];
-	float &idx1 = x[mat_width];
-	float &idx2 = x[mat_width * 2];
-	float &idx3 = x[mat_width * 3];
-	float &idx4 = x[mat_width * 4];
-	float &idx5 = x[mat_width * 5];
-	float &idx6 = x[mat_width * 6];
-	float &idx7 = x[mat_width * 7];
-	
-	float a0 = idx0 + idx7;
-	float a1 = idx1 + idx6;
-	float a2 = idx2 + idx5;
-	float a3 = idx3 + idx4;
-	float a5 = idx2 - idx5;
-	float a6 = idx1 - idx6;
-	float a7 = idx0 - idx7;
+	float a0 = *x[0] + *x[7];
+	float a1 = *x[1] + *x[6];
+	float a2 = *x[2] + *x[5];
+	float a3 = *x[3] + *x[4];
+	float a5 = *x[2] - *x[5];
+	float a6 = *x[1] - *x[6];
+	float a7 = *x[0] - *x[7];
 	
 	float b0 = a0 + a3;
 	float b1 = a1 + a2;
 	float b3 = a0 - a3;
-	float b4 = -(idx3 - idx4) - a5;
+	float b4 = -(*x[3] - *x[4]) - a5;
 	float b6 = a6 + a7;
 	
 	float A5_block = (b4 + b6) * A5;
@@ -143,38 +54,121 @@ void Arai::transformLineOG(float* &x, size_t mat_width) {
 	float e5 = d5 + a7;
 	float e7 = a7 - d5;
 	
-	idx0 = (b0 + b1) * S0;
-	idx1 = (e5 + d6) * S1;
-	idx2 = (d2 + b3) * S2;
-	idx3 = (e7 - d4) * S3;
-	idx4 = (b0 - b1) * S4;
-	idx5 = (d4 + e7) * S5;
-	idx6 = (b3 - d2) * S6;
-	idx7 = (e5 - d6) * S7;
+	*x[0] = (b0 + b1) * S0;
+	*x[1] = (e5 + d6) * S1;
+	*x[2] = (d2 + b3) * S2;
+	*x[3] = (e7 - d4) * S3;
+	*x[4] = (b0 - b1) * S4;
+	*x[5] = (d4 + e7) * S5;
+	*x[6] = (b3 - d2) * S6;
+	*x[7] = (e5 - d6) * S7;
 }
+
+Mat Arai::transform(Mat matrix) {
+	processRows(matrix.values);
+	processColumns(matrix.values);
+	return matrix;
+}
+
+void Arai::transformMT(float* values) {
+	processRows(values);
+	processColumns(values);
+}
+
+void Arai::processRows(float *values) {
+	for (int row = 0; row < 8; ++row)
+	{
+		float **currentLine = new float*[8];
+		
+		for (int column = 0; column < 8; ++column)
+		{
+			currentLine[column] = &values[row * 8 + column];
+		}
+		transformLine(currentLine);
+		
+		delete[] currentLine;
+	}
+}
+
+void Arai::processColumns(float *values) {
+	for (int column = 0; column < 8; ++column)
+	{
+		float **currentLine = new float*[8];
+		
+		for (int row = 0; row < 8; ++row)
+		{
+			currentLine[row] = &values[row * 8 + column];
+		}
+		transformLine(currentLine);
+		
+		delete[] currentLine;
+	}
+}
+
+
+#define ARAI(_0_, _1_, _2_, _3_, _4_, _5_, _6_, _7_) \
+float a0 = _0_ + _7_;\
+float a1 = _1_ + _6_;\
+float a2 = _2_ + _5_;\
+float a3 = _3_ + _4_;\
+float a5 = _2_ - _5_;\
+float a6 = _1_ - _6_;\
+float a7 = _0_ - _7_;\
+\
+float b0 = a0 + a3;\
+float b1 = a1 + a2;\
+float b3 = a0 - a3;\
+float b4 = -(_3_ - _4_) - a5;\
+float b6 = a6 + a7;\
+\
+float A5_block = (b4 + b6) * A5;\
+\
+float d2 = ((a1 - a2) + b3) * A1;\
+float d4 = -(b4 * A2) - A5_block;\
+float d5 = (a5 + a6) * A3;\
+float d6 = (b6 * A4) - A5_block;\
+\
+float e5 = d5 + a7;\
+float e7 = a7 - d5;\
+\
+_0_ = (b0 + b1) * S0;\
+_1_ = (e5 + d6) * S1;\
+_2_ = (d2 + b3) * S2;\
+_3_ = (e7 - d4) * S3;\
+_4_ = (b0 - b1) * S4;\
+_5_ = (d4 + e7) * S5;\
+_6_ = (b3 - d2) * S6;\
+_7_ = (e5 - d6) * S7;
+
+
+#define ARAI_ROW(data) ARAI( data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7] );
+
+#define ARAI_COL(data, width) ARAI( data[0], data[width], data[2 * width], data[3 * width], data[4 * width], data[5 * width], data[6 * width], data[7 * width] );
+
 
 void Arai::processRowsOG(float* &values, size_t numberOfPixels) {
 	float *rowPointer = &values[0];
 	size_t rowRepeat = numberOfPixels / 8;
 	
 	while (rowRepeat--) {
-		transformLineOG(rowPointer);
+		ARAI_ROW(rowPointer);
 		rowPointer += 8;
 	}
 }
 
 void Arai::processColumnsOG(float* &values, size_t image_width, size_t image_height) {
 	float *colPointer = &values[0];
-	size_t colRepeatY = image_height / 8;
-	size_t colRepeatX;
+	size_t lineSkip = image_width * 7; // 7 because one line was already processed
 	
+	unsigned short colRepeatX;
+	unsigned short colRepeatY = image_height / 8;
 	while (colRepeatY--) {
 		colRepeatX = image_width;
 		while (colRepeatX--) {
-			transformLineOG(colPointer, 8);
+			ARAI_COL(colPointer, image_width);
 			++colPointer;
 		}
-		colPointer += image_width * 7; // 8 (- 1 line) we already calculated
+		colPointer += lineSkip;
 	}
 }
 
