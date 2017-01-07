827ca3f8fe2a017ce2aae8982744f6b013e85177
relikd <info@relikd.de>
2017-01-07 17:37:44 +0100

    DCT & Arai decluttering

--- jpegenc/jpegenc/dtc/Arai.hpp
+++ jpegenc/jpegenc/dtc/Arai.hpp
@@ -2,23 +2,15 @@
 #define Arai_hpp
 
 #include <stdio.h>
-#include "Mat.hpp"
 
 struct Arai
 {
 public:
-    static Mat transform(Mat matrix);
-    static void transformLine(float **x);
-	
-	static void transformMT(float* values);
-	static void transformOG(float* &values, size_t image_width, size_t image_height);
+    static void transform(float* &values, size_t image_width, size_t image_height);
 	static void transformInlineTranspose(float* &values, size_t image_width, size_t image_height);
 private:
-    static void processRows(float *values);
-    static void processColumns(float *values);
-	
-	static void processRowsOG(float* &values, size_t numberOfPixels = 64);
-	static void processColumnsOG(float* &values, size_t image_width = 8, size_t image_height = 8);
+	static void processRows(float* &values, size_t numberOfPixels = 64);
+	static void processColumns(float* &values, size_t image_width = 8, size_t image_height = 8);
 };
 
 #endif /* Arai_hpp */
