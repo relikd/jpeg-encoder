bafcd740d8c25e10f0e704e9b5b3c93a72bf49f8
relikd <info@relikd.de>
2017-01-11 18:27:53 +0100

    oclAssert

--- jpegenc/jpegenc/opencl/oclAssert.h
+++ jpegenc/jpegenc/opencl/oclAssert.h
@@ -1,67 +1,67 @@
 #ifndef OCL_ASSERT_H
 #define OCL_ASSERT_H
 
-#define oclAssert(x) oclCheckError(x, CL_SUCCESS, __FILE__ , __LINE__);
+#define oclAssert(x) oclCheckError(x, __FILE__ , __LINE__);
 
 #include <stdlib.h>
 #include <stdio.h>
+static const char* errorArray[] = {
+	"CL_SUCCESS",
+	"CL_DEVICE_NOT_FOUND",
+	"CL_DEVICE_NOT_AVAILABLE",
+	"CL_COMPILER_NOT_AVAILABLE",
+	"CL_MEM_OBJECT_ALLOCATION_FAILURE",
+	"CL_OUT_OF_RESOURCES",
+	"CL_OUT_OF_HOST_MEMORY",
+	"CL_PROFILING_INFO_NOT_AVAILABLE",
+	"CL_MEM_COPY_OVERLAP",
+	"CL_IMAGE_FORMAT_MISMATCH",
+	"CL_IMAGE_FORMAT_NOT_SUPPORTED",
+	"CL_BUILD_PROGRAM_FAILURE",
+	"CL_MAP_FAILURE",
+	"","","","","","","","","","","","","","","","","",
+	"CL_INVALID_VALUE",
+	"CL_INVALID_DEVICE_TYPE",
+	"CL_INVALID_PLATFORM",
+	"CL_INVALID_DEVICE",
+	"CL_INVALID_CONTEXT",
+	"CL_INVALID_QUEUE_PROPERTIES",
+	"CL_INVALID_COMMAND_QUEUE",
+	"CL_INVALID_HOST_PTR",
+	"CL_INVALID_MEM_OBJECT",
+	"CL_INVALID_IMAGE_FORMAT_DESCRIPTOR",
+	"CL_INVALID_IMAGE_SIZE",
+	"CL_INVALID_SAMPLER",
+	"CL_INVALID_BINARY",
+	"CL_INVALID_BUILD_OPTIONS",
+	"CL_INVALID_PROGRAM",
+	"CL_INVALID_PROGRAM_EXECUTABLE",
+	"CL_INVALID_KERNEL_NAME",
+	"CL_INVALID_KERNEL_DEFINITION",
+	"CL_INVALID_KERNEL",
+	"CL_INVALID_ARG_INDEX",
+	"CL_INVALID_ARG_VALUE",
+	"CL_INVALID_ARG_SIZE",
+	"CL_INVALID_KERNEL_ARGS",
+	"CL_INVALID_WORK_DIMENSION",
+	"CL_INVALID_WORK_GROUP_SIZE",
+	"CL_INVALID_WORK_ITEM_SIZE",
+	"CL_INVALID_GLOBAL_OFFSET",
+	"CL_INVALID_EVENT_WAIT_LIST",
+	"CL_INVALID_EVENT",
+	"CL_INVALID_OPERATION",
+	"CL_INVALID_GL_OBJECT",
+	"CL_INVALID_BUFFER_SIZE",
+	"CL_INVALID_MIP_LEVEL",
+	"CL_INVALID_GLOBAL_WORK_SIZE",
+};
+static const int errorCount = sizeof(errorArray) / sizeof(errorArray[0]);
 
-inline void oclCheckError(int value, int compare, const char* cFile, const int iLine) {
-    if (value != compare) {
-        static const char* errorArray[] = {
-			"CL_SUCCESS",
-			"CL_DEVICE_NOT_FOUND",
-			"CL_DEVICE_NOT_AVAILABLE",
-			"CL_COMPILER_NOT_AVAILABLE",
-			"CL_MEM_OBJECT_ALLOCATION_FAILURE",
-			"CL_OUT_OF_RESOURCES",
-			"CL_OUT_OF_HOST_MEMORY",
-			"CL_PROFILING_INFO_NOT_AVAILABLE",
-			"CL_MEM_COPY_OVERLAP",
-			"CL_IMAGE_FORMAT_MISMATCH",
-			"CL_IMAGE_FORMAT_NOT_SUPPORTED",
-			"CL_BUILD_PROGRAM_FAILURE",
-			"CL_MAP_FAILURE",
-			"","","","","","","","","","","","","","","","","",
-			"CL_INVALID_VALUE",
-			"CL_INVALID_DEVICE_TYPE",
-			"CL_INVALID_PLATFORM",
-			"CL_INVALID_DEVICE",
-			"CL_INVALID_CONTEXT",
-			"CL_INVALID_QUEUE_PROPERTIES",
-			"CL_INVALID_COMMAND_QUEUE",
-			"CL_INVALID_HOST_PTR",
-			"CL_INVALID_MEM_OBJECT",
-			"CL_INVALID_IMAGE_FORMAT_DESCRIPTOR",
-			"CL_INVALID_IMAGE_SIZE",
-			"CL_INVALID_SAMPLER",
-			"CL_INVALID_BINARY",
-			"CL_INVALID_BUILD_OPTIONS",
-			"CL_INVALID_PROGRAM",
-			"CL_INVALID_PROGRAM_EXECUTABLE",
-			"CL_INVALID_KERNEL_NAME",
-			"CL_INVALID_KERNEL_DEFINITION",
-			"CL_INVALID_KERNEL",
-			"CL_INVALID_ARG_INDEX",
-			"CL_INVALID_ARG_VALUE",
-			"CL_INVALID_ARG_SIZE",
-			"CL_INVALID_KERNEL_ARGS",
-			"CL_INVALID_WORK_DIMENSION",
-			"CL_INVALID_WORK_GROUP_SIZE",
-			"CL_INVALID_WORK_ITEM_SIZE",
-			"CL_INVALID_GLOBAL_OFFSET",
-			"CL_INVALID_EVENT_WAIT_LIST",
-			"CL_INVALID_EVENT",
-			"CL_INVALID_OPERATION",
-			"CL_INVALID_GL_OBJECT",
-			"CL_INVALID_BUFFER_SIZE",
-			"CL_INVALID_MIP_LEVEL",
-			"CL_INVALID_GLOBAL_WORK_SIZE",
-		};
-		
-		const int errorCount = sizeof(errorArray) / sizeof(errorArray[0]);
+
+inline void oclCheckError(int value, const char* cFile, const int iLine) {
+    if (value != 0) { // CL_SUCCESS
 		const int index = -value;
-		const char* errorString = (index >= 0 && index < errorCount) ? errorArray[index] : "Unspecified Error";
+		const char* errorString = (index > 0 && index < errorCount) ? errorArray[index] : "Unspecified Error";
 		
         printf("\n!!! OCL Error # %i (%s) at line %i , in file %s !!!\n\n", value, errorString, iLine, cFile);
 		exit(value);
