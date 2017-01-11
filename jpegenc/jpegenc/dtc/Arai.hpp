ff647ed16ad5547206985e8453368f1a6988b6f8
Oleg Geier <oleg@relikd.de>
2017-01-11 17:29:23 +0100

    Pass pointer instead of pointer reference (because its faster)

--- jpegenc/jpegenc/dtc/Arai.hpp
+++ jpegenc/jpegenc/dtc/Arai.hpp
@@ -6,11 +6,11 @@
 struct Arai
 {
 public:
-    static void transform(float* &values, size_t image_width, size_t image_height);
-	static void transformInlineTranspose(float* &values, size_t image_width, size_t image_height);
+    static void transform(float* values, size_t image_width, size_t image_height);
+	static void transformInlineTranspose(float* values, size_t image_width, size_t image_height);
 private:
-	static void processRows(float* &values, size_t numberOfPixels = 64);
-	static void processColumns(float* &values, size_t image_width = 8, size_t image_height = 8);
+	static void processRows(float* values, size_t numberOfPixels = 64);
+	static void processColumns(float* values, size_t image_width = 8, size_t image_height = 8);
 };
 
 #endif /* Arai_hpp */
