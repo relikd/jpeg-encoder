061aa5160d8256df744b27bfff250d106c424a0a
relikd <info@relikd.de>
2017-01-12 04:07:38 +0100

    Reuse kernel

--- jpegenc/jpegenc/opencl/OCL_DCT.h
+++ jpegenc/jpegenc/opencl/OCL_DCT.h
@@ -5,8 +5,8 @@
 
 class OCL_DCT {
 public:
-	static void separated(float* &matrix, size_t width, size_t height);
-	static void arai(float* &matrix, size_t width, size_t height);
+	static void separated(float* matrix, size_t width, size_t height);
+	static void arai(float* matrix, size_t width, size_t height);
 };
 
 #endif /* OCL_DCT_hpp */
